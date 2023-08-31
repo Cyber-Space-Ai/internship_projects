5108ef2297e5363a63f245e16ee2ff50fa761b44
YSALMAEZAHRAOUI <131591559+YSALMAEZAHRAOUI@users.noreply.github.com>
2023-08-31 15:00:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,64 @@
-require('dotenv').config();
-const {MongoClient}=require('mongodb');
-const client =new MongoClient (process.env.Mongo_URL);
+const express = require('express');
+const mongodb = require('mongodb');
 
-async function main(){
-    await client.connect ();
-    console.log('connection ok');
-    const db=client.db('internship');
-    const collection= db.collection ('document');
-    try {
-        
-        const inserdata = await collection.insertMany([
-{
-    name:'zahra'
-}
-        ]);
-        console.log ('document inser=>',inserdata);
+const app = express();
+const port = 3000;
 
-    }catch(e){ throw e ;}
+// MongoDB Atlas connection details
+const mongoURL = 'mongodb+srv://salima:n2Dv34yFApQNYmOx@cluster0.d8bcbdk.mongodb.net/?retryWrites=true&w=majority';
 
+// Database and collection names
+const dbName = 'internship';
+const collectionName = 'document';
 
-_};
-main()
-.then(console.log)
- .catch(console.error)
-.finally(()=>client.close());
+// Middleware to parse the request body
+app.use(express.urlencoded({ extended: true }));
+
+// Route handler for form submission
+app.post('/insert', (req, res) => {
+  // Extract form data
+  const { nom, prenom, email, telephone, niveau, genre, durer, service, age } = req.body;
+
+  // Create a new document object
+  const document = {
+    nom,
+    prenom,
+    email,
+    telephone,
+    niveau,
+    genre,
+    durer,
+    service,
+    age: parseInt(age),
+  };
+
+  // Connect to MongoDB Atlas
+  mongodb.MongoClient.connect(mongoURL, { useUnifiedTopology: true }, (err, client) => {
+    if (err) {
+      console.error(err);
+      return res.sendStatus(500);
+    }
+
+    // Select the database
+    const db = client.db(dbName);
+
+    // Insert the document into the collection
+    db.collection(collectionName).insertOne(document, (err) => {
+      if (err) {
+        console.error(err);
+        return res.sendStatus(500);
+      }
+
+      // Close the MongoDB connection
+      client.close();
+
+      // Redirect the user or send a success message
+      res.send('Form submitted successfully!');
+    });
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
